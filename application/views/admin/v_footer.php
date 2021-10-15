0ec0b9950dfd508eaeeb8b4b151aef2b1710a065
jariguest <guest@jari.co.id>
2021-10-15 14:38:16 +0700

    master

---
+++ application/views/admin/v_footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy;  <?php echo date("Y") ?> Desa Babakan Kecamatan tenjo | Made with <span i class="fa fa-heart" style="color:#DC3545"> </span> by Universitas Pamulang </a>
+  </footer>
